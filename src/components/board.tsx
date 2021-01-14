ea3b6f1995af8e01b855308a9acdd9722b5d450c
capener-cade <cadec@ifit.com>
2021-01-13 22:43:33 -0700

    Moved filtering logic into column component.

--- src/components/board.tsx
+++ src/components/board.tsx
@@ -1,78 +1,31 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import Column from './column'
-import axios from 'axios'
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 
-const cards = [
-    {
-        "id":14,
-        "column": "ToDo",
-        "title": "Go Outside"
-    },
-    {
-        "id":2,
-        "column": "Doing",
-        "title": "Run"
-    },
-    {
-        "id":1,
-        "column": "Done",
-        "title": "Lift"
-    },
-    {
-        "id":12,
-        "column": "Done",
-        "title": "Walk"
-    }
-]
-
-
 
 function Board() {
-    const [cardData, setCardData] = useState(cards)
 
-    useEffect( ()=>{
-         const fetchData = async () => {
-             const response = await axios.get('http://localhost:3001/api/board/1')
-             setCardData(response.data.cards)
-         }
-         fetchData()
-    }, [])
- 
-    const backlogData = cardData.filter((currentValue, index, array) => {
-        console.log(cardData)
-        return currentValue.column === "Backlog"
-    })
-    const toDoData = cardData.filter((currentValue, index, array) => {
-        return currentValue.column === "ToDo"
-    })
-    const doingData = cardData.filter((currentValue, index, array) => {
-        return currentValue.column === "Doing"
-    })
-    const doneData = cardData.filter((currentValue, index, array) => {
-        return currentValue.column === "Done"
-    })
 
     return (
         <Container>
             <Row>
                 <Col>
                 <h2>Backlog</h2>
-                <Column column={"Backlog"} cards={backlogData}/>
+                <Column column={"Backlog"}/>
                 </Col>
                 <Col>
                 <h2>ToDo</h2>
-                <Column column={"ToDo"} cards={toDoData}/>
+                <Column column={"ToDo"}/>
                 </Col>
                 <Col>
                 <h2>Doing</h2>
-                <Column column={"Doing"} cards={doingData}/>
+                <Column column={"Doing"} />
                 </Col>
                 <Col>
                 <h2>Done</h2>
-                <Column column={"Done"} cards={doneData}/>
+                <Column column={"Done"}/>
                 </Col>
             </Row>
         </Container>
--- src/components/column.tsx
+++ src/components/column.tsx
@@ -1,19 +1,35 @@
-import React from 'react';
+import React, {useEffect, useState}from 'react';
 import axios from 'axios'
 import Container from 'react-bootstrap/Container'
 import Button from 'react-bootstrap/Button'
 import Card from 'react-bootstrap/Card'
+import PropTypes from 'prop-types'
 
-
+Column.propTypes = {
+    column: PropTypes.string,
+    cards: PropTypes.array
+}
 function Column(props: any) {
     const taskList = ['Backlog', 'ToDo', 'Doing', 'Done']
+    const [cardData, setCardData] = useState([])
     const erase = () => {console.log(`delete the ${props.cards.title}`)}
     const add = () => {console.log(`add a card to ${props.column}`)}
     const columnSelect = (e: any) => {console.log(e.target.value)}
 
+        useEffect( ()=>{
+         const fetchData = async () => {
+             const response = await axios.get('http://localhost:3001/api/board/1')
+             setCardData(response.data.cards)
+         }
+         fetchData()
+    }, [])
+
+    //filter here 
+    const filteredCards = cardData.filter((cards) => cards["column"] === props.column);
+
     return (
         <Container>
-            {props.cards.map((card: { id: string | number | null | undefined; title: React.ReactNode; }) => {
+            {filteredCards.map((card: { id: string | number | null | undefined; title: React.ReactNode; }) => {
             return <Card key={card.id}>
                     <Card.Title>{card.title}</Card.Title>
                             <select  onChange={columnSelect}>
