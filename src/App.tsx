52da1b886bd461708b13d7031cd415e2cb61fc06
capener-cade <cadec@ifit.com>
2021-01-28 08:48:54 -0700

    Set up prettier, formatted files in src

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Board from './components/board'
-import axios from 'axios'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Button from 'react-bootstrap/Button'
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Board from "./components/board";
+import axios from "axios";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Button from "react-bootstrap/Button";
 
 function App() {
-
   const fetch = () => {
-    axios.get('http://localhost:3001/api/ping')
-      .then(response => console.log(response))
-  }
+    axios
+      .get("http://localhost:3001/api/ping")
+      .then((response) => console.log(response));
+  };
   return (
     <div className="App">
-      <Board/>
+      <Board />
     </div>
   );
 }
