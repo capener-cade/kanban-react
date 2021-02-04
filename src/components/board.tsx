d1149b8c0962f9c3fbe4d2e6412bdda6760948d2
capener-cade <cadec@ifit.com>
2021-02-03 21:26:23 -0700

    Revert "Set up prettier, formatted files in src" Reorganized board component to refresh on changes.

--- src/components/board.tsx
+++ src/components/board.tsx
@@ -1,89 +1,87 @@
-import React, { useState, useEffect } from "react";
-import Column from "./column";
-import axios from "axios";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import Button from "react-bootstrap/Button";
+import React, {useState, useEffect} from 'react';
+import Column from './column'
+import axios from 'axios'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Button from 'react-bootstrap/Button'
+
+
 
 type ColumnCard = {
-  id: number;
-  column: string;
-  title: string;
-};
+    id: number;
+    column: string;
+    title: string;
+}
 
 function Board() {
-  //TODO: add a "Create Card" button here.  All it needs to do is create a new card for the backlog column and add to the array and set the state
-  //TODO: remve the move to drop down for now
-  //TODO: get all the card here.  push them into a state for each column, pass the specific array down to the column
-  const [cardData, setCardData] = useState([]);
-  const [backlogColumnData, setBacklogColumnData] = useState([]);
-  const [toDoColumnData, setToDoColumnData] = useState([]);
-  const [doingColumnData, setDoingColumnData] = useState([]);
-  const [doneColumnData, setDoneColumnData] = useState([]);
+//TODO: add a "Create Card" button here.  All it needs to do is create a new card for the backlog column and add to the array and set the state
+//TODO: remve the move to drop down for now
+//TODO: get all the card here.  push them into a state for each column, pass the specific array down to the column
+const [cardData, setCardData] = useState([])
+const [backlogColumnData, setBacklogColumnData] = useState([])
+const [toDoColumnData, setToDoColumnData] = useState([])
+const [doingColumnData, setDoingColumnData] = useState([])
+const [doneColumnData, setDoneColumnData] = useState([])
 
-  const addCard = () => {
+const getCards = async (boardId: number): Promise<any> => {
+    const response = await axios.get(`http://localhost:3001/api/board/${boardId}/cards`)
+    return response.data
+}
+
+const setCardsState = (cards: any): void => {
+    setCardData(cards)
+    setBacklogColumnData(cards.filter((card: ColumnCard) => card.column.toUpperCase() === "BACKLOG"));
+    setToDoColumnData(cards.filter((card: ColumnCard) => card.column.toUpperCase() === "TODO"));
+    setDoingColumnData(cards.filter((card: ColumnCard) => card.column.toUpperCase() === "DOING"));
+    setDoneColumnData(cards.filter((card: ColumnCard) => card.column.toUpperCase() === "DONE"));
+}
+
+const refreshBoard = async(boardId: number): Promise<void> => {
+    const cards = await getCards(boardId);
+    setCardsState(cards);
+}
+
+const addCard = async () => {
     const newCard = {
-      boardId: 1,
-      column: "Backlog",
-      title: "Add a New Card",
-    };
-    axios.post("http://localhost:3001/api/board/1/cards", newCard);
-  };
+            "boardId":1,
+            "column": "Backlog",
+            "title": "Add a New Card"
+    }
+    await axios.post('http://localhost:3001/api/board/1/cards', newCard)
+    await refreshBoard(1);
+}
 
-  useEffect(() => {
+useEffect( ()=>{
     const fetchData = async () => {
-      const response = await axios.get(
-        "http://localhost:3001/api/board/1/cards"
-      );
-      setCardData(response.data);
-      setBacklogColumnData(
-        response.data.filter(
-          (card: ColumnCard) => card.column.toUpperCase() === "BACKLOG"
-        )
-      );
-      setToDoColumnData(
-        response.data.filter(
-          (card: ColumnCard) => card.column.toUpperCase() === "TODO"
-        )
-      );
-      setDoingColumnData(
-        response.data.filter(
-          (card: ColumnCard) => card.column.toUpperCase() === "DOING"
-        )
-      );
-      setDoneColumnData(
-        response.data.filter(
-          (card: ColumnCard) => card.column.toUpperCase() === "DONE"
-        )
-      );
-    };
-    fetchData();
-  }, []);
+        await refreshBoard(1);
+    }
+    fetchData()
+}, [])
 
-  return (
-    <Container>
-      <Row>
-        <Col>
-          <h2>Backlog</h2>
-          <Column column={"Backlog"} cards={backlogColumnData} />
-          <Button onClick={addCard}>Create A Card</Button>
-        </Col>
-        <Col>
-          <h2>ToDo</h2>
-          <Column column={"ToDo"} cards={toDoColumnData} />
-        </Col>
-        <Col>
-          <h2>Doing</h2>
-          <Column column={"Doing"} cards={doingColumnData} />
-        </Col>
-        <Col>
-          <h2>Done</h2>
-          <Column column={"Done"} cards={doneColumnData} />
-        </Col>
-      </Row>
-    </Container>
-  );
+    return (
+        <Container>
+            <Row>
+                <Col>
+                <h2>Backlog</h2>
+                <Column column={"Backlog"} cards={backlogColumnData} refreshBoard={refreshBoard}/>
+                <Button onClick={addCard}>Create A Card</Button>
+                </Col>
+                <Col>
+                <h2>ToDo</h2>
+                <Column column={"ToDo"} cards={toDoColumnData} refreshBoard={refreshBoard}/>
+                </Col>
+                <Col>
+                <h2>Doing</h2>
+                <Column column={"Doing"} cards={doingColumnData} refreshBoard={refreshBoard}/>
+                </Col>
+                <Col>
+                <h2>Done</h2>
+                <Column column={"Done"} cards={doneColumnData} refreshBoard={refreshBoard}/>
+                </Col>
+            </Row>
+        </Container>
+    );
 }
 
 export default Board;
--- src/components/column.tsx
+++ src/components/column.tsx
@@ -1,67 +1,44 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
-import Container from "react-bootstrap/Container";
-import Button from "react-bootstrap/Button";
-import Card from "react-bootstrap/Card";
-import PropTypes from "prop-types";
+import React, {useEffect, useState}from 'react';
+import axios from 'axios'
+import Container from 'react-bootstrap/Container'
+import Button from 'react-bootstrap/Button'
+import Card from 'react-bootstrap/Card'
+import PropTypes from 'prop-types'
 
 type ColumnCard = {
-  _id: number;
-  title: string;
-  boardId: number;
-};
+    _id: number;
+    title: string;
+    boardId: number
+}
 
-type Props = {
-  column: string;
-  cards: ColumnCard[];
-};
+interface Props {
+    column: string;
+    cards: ColumnCard[];
+    refreshBoard(boardId: number): Promise<void>;
+}
 
 function Column(props: Props) {
-  const { column, cards } = props;
-  // const taskList = ['Backlog', 'ToDo', 'Doing', 'Done']
-  const erase = (id: string | number | null | undefined, boardId: number) => {
-    axios.delete(`http://localhost:3001/api/board/${boardId}/${id}`);
-  };
-  const move = (id: string | number | null | undefined, boardId: number) => {
-    axios.put(`http://localhost:3001/api/board/${boardId}/${id}`);
-  };
+    const {column, cards} = props;
+    // const taskList = ['Backlog', 'ToDo', 'Doing', 'Done']
+     const erase = async(id: string | number | null | undefined, boardId: number): Promise<void> =>  { 
+         await axios.delete(`http://localhost:3001/api/board/${boardId}/${id}`);
+         await props.refreshBoard(boardId);
+      }
 
-  return (
-    <Container>
-      {cards.map(
-        (card: {
-          _id: string | number | null | undefined;
-          title: React.ReactNode;
-          boardId: number;
-        }) => {
-          return (
-            <Card key={card._id}>
-              <Card.Title>{card.title}</Card.Title>
-              <Container>
-                <Button
-                  size="sm"
-                  variant="outline-danger"
-                  onClick={(event: React.MouseEvent<HTMLElement>) => {
-                    erase(card._id, card.boardId);
-                  }}
-                >
-                  Delete Card
-                </Button>
-                <Button
-                  size="sm"
-                  onClick={(event: React.MouseEvent<HTMLElement>) => {
-                    move(card._id, card.boardId);
-                  }}
-                >
-                  Move
-                </Button>
-              </Container>
-            </Card>
-          );
-        }
-      )}
-    </Container>
-  );
+    return (
+        <Container>
+            {cards.map((card: { _id: string | number | null | undefined; title: React.ReactNode; boardId: number }) => {
+            return <Card key={card._id}>
+                    <Card.Title>{card.title}</Card.Title>
+                        <Container>
+                            <Button size="sm" variant="outline-danger" onClick={(event: React.MouseEvent<HTMLElement>) => {
+                                erase(card._id, card.boardId)
+                            }}>Delete Card</Button>
+                        </Container>
+                    </Card>
+            })}
+        </Container>
+    );
 }
 
 export default Column;
