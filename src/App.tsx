ba7a10ac529b089759aa58cae4158c5dd4ec3d56
capener-cade <cadec@ifit.com>
2021-02-10 21:07:10 -0700

    Removed unused imports from App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Board from './components/board'
-import axios from 'axios'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Button from 'react-bootstrap/Button'
 
 function App() {
   return (
