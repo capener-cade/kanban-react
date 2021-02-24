a3b11c9c682e0caa5c5ad9c7d4e72a29a175baa7
capener-cade <cadec@ifit.com>
2021-02-23 23:23:26 -0700

    Added dark theme to include main background

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,27 @@
-import React from 'react';
-import './App.css';
-import Board from './components/board'
-import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
-import {AppBar, Toolbar, Typography } from "@material-ui/core"
+import React from "react";
+import "./App.css";
+import Board from "./components/board";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+import { AppBar, Toolbar, Typography } from "@material-ui/core";
 
 const theme = createMuiTheme({
   palette: {
-    primary: {
-      light: '#ffffff',
-      main: '#cfd8dc',
-      dark: '#9ea7aa',
-      contrastText: '#000000',
-    },
-    secondary: {
-      light: '#718792',
-      main: '#455a64',
-      dark: '#1c313a',
-      contrastText: '#ffffff',
-    }
-  }
-})
+    type: "dark",
+  },
+});
 
 function App() {
   return (
     <div className="App">
       <ThemeProvider theme={theme}>
-        <div style={{margin:"100px"}}>
-        <AppBar style={{display:"flex", flexDirection:"row", alignItems:"center"}}> 
-        <Typography style={{paddingLeft:"50px", height:"100%", margin:"20px 0"}}variant="h5">Trello Clone</Typography>
-        </AppBar>
+        <div style={{ margin: "100px" }}>
+          <AppBar style={{ display: "flex", flexDirection: "row", alignItems: "center" }}>
+            <Typography style={{ paddingLeft: "50px", height: "100%", margin: "20px 0" }} variant="h5">
+              Trello Clone
+            </Typography>
+          </AppBar>
         </div>
-      <Board/>
+        <Board />
       </ThemeProvider>
     </div>
   );
--- src/components/modalForm.tsx
+++ src/components/modalForm.tsx
@@ -1,53 +1,65 @@
-import React, {useState, useEffect} from 'react';
-import axios from 'axios'
-import {  Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, TextField, Button, ButtonGroup, FormGroup } from '@material-ui/core';
-
-
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import {
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  TextField,
+  Button,
+  ButtonGroup,
+  FormGroup,
+} from "@material-ui/core";
 
 function ModalForm(props: any) {
-    const [show, setShow] = useState(false);
-    const [titleValue, setTitleValue] = useState('')
+  const [show, setShow] = useState(false);
+  const [titleValue, setTitleValue] = useState("");
 
-    const handleClose = () => {setShow(false)};
-    const handleShow = () => setShow(true);
+  const handleClose = () => {
+    setShow(false);
+  };
+  const handleShow = () => setShow(true);
 
-    const addCard = async () => {
-        const newCard = {
-                "boardId":1,
-                "column": props.column,
-                "title": titleValue
-        }
-        await axios.post('http://localhost:3001/api/board/1/cards', newCard)
-        await props.refreshBoard(1);
-        handleClose()
-    }
+  const addCard = async () => {
+    const newCard = {
+      boardId: 1,
+      column: props.column,
+      title: titleValue,
+    };
+    await axios.post("http://localhost:3001/api/board/1/cards", newCard);
+    await props.refreshBoard(1);
+    handleClose();
+  };
 
-    return (
-        <>
+  return (
+    <>
       <ButtonGroup color="secondary" size="small" onClick={handleShow}>
         <Button>Add A Card</Button>
       </ButtonGroup>
-      
-      <Dialog open={show} onClose={handleClose}> 
-          <DialogTitle>Add A Card</DialogTitle>
-        <DialogActions style={{padding:"15px"}}>
-        <FormGroup>
-               <TextField autoFocus margin="dense" id="title" type="title" label="Title" value={titleValue}  onChange={e => setTitleValue(e.target.value)}/>
-             </FormGroup>
+
+      <Dialog open={show} onClose={handleClose}>
+        <DialogTitle>Add A Card</DialogTitle>
+        <DialogActions style={{ padding: "15px" }}>
+          <FormGroup>
+            <TextField
+              autoFocus
+              margin="dense"
+              id="title"
+              type="title"
+              label="Title"
+              value={titleValue}
+              onChange={(e) => setTitleValue(e.target.value)}
+            />
+          </FormGroup>
         </DialogActions>
         <DialogActions>
-          <Button onClick={handleClose}>
-            Close
-          </Button>
-          <Button onClick={addCard}>
-            Add Card
-          </Button>
+          <Button onClick={handleClose}>Close</Button>
+          <Button onClick={addCard}>Add Card</Button>
         </DialogActions>
       </Dialog>
-      
     </>
-    );
+  );
 }
 
 export default ModalForm;
